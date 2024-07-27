dc4f39a5fddc708599dbdb163286685351a78ad0
sachin-source <sachinmgvt@gmail.com>
2024-07-27 15:25:02 +0530

    add actions

--- app/bears/_store/index.ts
+++ app/bears/_store/index.ts
@@ -2,10 +2,21 @@ import { create } from 'zustand';
 
 // State types
 interface States {
-  bears: number;
+    bears: number;
 }
 
-// useBearStore
-export const useBearStore = create<States>(() => ({
-  bears: 0,
+// Action types
+interface Actions {
+    increase: () => void;
+    decrease: () => void;
+}
+
+const increase = (state:States) => ({ bears : state.bears + 1 })
+const decrease = (state:States) => ({ bears : state.bears - 1 })
+
+// create store and actions
+export const useBearStore = create<States & Actions>((set) => ({
+    bears: 0,
+    increase: () => set(increase),
+    decrease: () => set(decrease),
 }));
