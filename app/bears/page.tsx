63b1e4492ca3e64f0abfd28c5504ee23c518fd0d
sachin-source <sachinmgvt@gmail.com>
2024-07-27 15:34:38 +0530

    integrate actions to UI

--- app/bears/page.tsx
+++ app/bears/page.tsx
@@ -3,14 +3,14 @@
 import { useBearStore } from "./_store";
 
 export default function Page() {
-    const store = useBearStore()
+    const { bears, increase, decrease } = useBearStore()
     return (
       <main>
         <h1>Bears</h1>
-        <p>How many Bears are in our Store? {store.bears}</p>
+        <p>How many Bears are in our Store? {bears}</p>
   
-        <button>Increase</button>
-        <button>Decrease</button>
+        <button onClick={increase} >Increase</button>
+        <button onClick={decrease} >Decrease</button>
       </main>
     );
   }
