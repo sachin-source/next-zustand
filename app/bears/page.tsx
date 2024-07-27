a241cafbc8d774dfee55c9b8d9e252cda34d0edf
sachin-source <sachinmgvt@gmail.com>
2024-07-27 15:12:29 +0530

    initiating the bear page

---
+++ app/bears/page.tsx
@@ -0,0 +1,11 @@
+export default function Page() {
+    return (
+      <main>
+        <h1>Bears</h1>
+        <p>How many Bears are in our Store? {0}</p>
+  
+        <button>Increase</button>
+        <button>Decrease</button>
+      </main>
+    );
+  }
